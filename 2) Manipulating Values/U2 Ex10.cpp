b7e22ad691a9c3f3828d8e1efe8553c53e458b34
mahinm23 <126404273+mahinm23@users.noreply.github.com>
2026-06-21 18:47:59 +0600

    Dice Roll 2

--- 2) Manipulating Values/U2 Ex10.cpp
+++ 2) Manipulating Values/U2 Ex10.cpp
@@ -3,7 +3,11 @@
 int main() {
     srand(time(0));
     int roll = rand() % 6 + 5;
-    std::cout << roll << std::endl;
+    std::cout << roll << " Roll 1" << std::endl;
+    std::cout << " " << std::endl;
+
+    int roll2 = (rand() % 6)*2 + 1;
+    std::cout << roll2 << " Roll 2" << std::endl;
 
     return 0;
 }
